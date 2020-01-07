67d22695db25b7af5e2043e4a1f2bc73e86b91f3
bornafana <borna.fana@gmail.com>
2020-01-07 16:14:01 +0400

    Prototype polish
    - Added set amount of Jump and Dashes the player can make
    - Player faces correct direction when moving
    - Colliders are not buggy (changed from box to capsule)
    - Game recognizes when player is grounded properly because we are now using 2 separate colliders on the player
    - Added simple gem collection without UI or counting
    - Added simple prototype audio for demo

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,40 +2,67 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Player : MonoBehaviour
 {
     public bool canWalk = false;
     public float movementSpeed;
 
+    public int totalDashes = 1;
     public Vector2 dashSpeed;
-    [SerializeField] private float jumpPower;
+    public int totalJumps = 1;
+    public float jumpPower;
     public float positiveCutOffAngle;
-    public float negativeCutOffAngle;
 
     public bool inverseMovement;
     public FloatingJoystick joystick;
 
+    [Header("Audio")]
+    public AudioSource jumpSFX;
+    public AudioSource LandSFX;
+    public AudioSource gemSFX;
+    public List<AudioSource> dashSFX;
+    private AudioSource dashAudioParent;
+
+    [Header("Debug")]
+    public int remainingDashes;
+    public int remainingJumps;
+
+    private GroundDetector groundDetector;
+    private SpriteRenderer spriteRenderer;
     private float powerDivident = 200f; // Brings dragDistance to a smaller number so that player doesn't jump too far. Normalize didn't work properly.
     private Rigidbody2D rb;
     private Vector2 direction;
     private bool isGrounded = true;
 
+
+
     private void Start()
     {
+        remainingJumps = totalJumps;
+        remainingDashes = totalDashes;
+
+        foreach (Transform child in transform)
+        {
+            groundDetector = child.GetComponent<GroundDetector>();
+        }
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
         rb = GetComponent<Rigidbody2D>();
+        dashAudioParent = dashSFX[0].transform.parent.GetComponent<AudioSource>();
         DragIndicator.OnDrag += HandleInput;
 
         if (powerDivident <= 0f)
             powerDivident = 100f;
 
-        //joystick.dashEvent.AddListener(Dash);
+        groundDetector.groundEvent.AddListener(Grounded);
         joystick.walkEvent.AddListener(Walk);
     }
 
     private void Update()
     {
-        //Debug.Log("Grounded: " + isGrounded);
+        Debug.Log("Grounded: " + isGrounded);
     }
 
     private void HandleInput(Vector2 dragDistance)
@@ -52,22 +79,51 @@ public class Player : MonoBehaviour
 
     private void Jump(Vector2 dragDistance)
     {
-        rb.AddForce((dragDistance / powerDivident) * jumpPower, ForceMode2D.Impulse);
         Debug.Log("JUMP");
+
+        if (remainingJumps < 1)
+            return;
+
+        spriteRenderer.flipX = dragDistance.x < 0;
+
+        rb.AddForce((dragDistance / powerDivident) * jumpPower, ForceMode2D.Impulse);
+        remainingJumps--;
+
+        jumpSFX.Play();
     }
 
     private void Dash(Vector2 dragDistance)
     {
+        Debug.Log("DASH");
+
+        if(remainingDashes < 1)
+            return;
+
+        spriteRenderer.flipX = dragDistance.x < 0;
+
         Vector2 dash = dashSpeed;
 
-        if (dragDistance.x < 0f)    //Handles when swiping 
+        if (dragDistance.x < 0f) //Handles when swiping other direction
+        {
             dash.x = -dash.x;
+        }
 
         if (dash.y < 0f)    //So that player never dashes downwards (may change later)
             dash.y = 0f;
 
         rb.AddForce(dash, ForceMode2D.Impulse);
-        Debug.Log("DASH");
+        remainingDashes--;
+
+        var rand = Random.Range(0, dashSFX.Count - 1);
+
+        if (dashAudioParent.isPlaying)
+        {
+            dashAudioParent.Stop();
+        }
+
+        dashAudioParent.clip = dashSFX[rand].clip;
+        dashAudioParent.Play();
+
 
     }
 
@@ -77,9 +133,6 @@ public class Player : MonoBehaviour
             direction = Vector2.left * horizontal;
         else
             direction = Vector2.right * horizontal;
-
-       
-
 
         rb.AddForce(direction * dashSpeed, ForceMode2D.Impulse);
     }
@@ -94,22 +147,31 @@ public class Player : MonoBehaviour
         else
             direction = Vector2.right * horizontal;
 
-        if (direction.x < 0)
-            GetComponent<SpriteRenderer>().flipX = true;
-        else
-            GetComponent<SpriteRenderer>().flipX = false;
+        spriteRenderer.flipX = direction.x < 0;
 
         rb.AddForce(direction * movementSpeed * Time.fixedDeltaTime, ForceMode2D.Impulse);
     }
 
-    private void OnColliderEnter(Collider col)
+    private void Grounded()
     {
         isGrounded = true;
+        LandSFX.Play();
+        remainingJumps = totalJumps;
+        remainingDashes = totalDashes;
     }
 
-    private void OnColliderExit(Collider col)
+    private void OnCollisionExit2D(Collision2D col)
     {
         isGrounded = false;
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.CompareTag("Gem"))
+        {
+            gemSFX.Play();
+            Destroy(col.gameObject);
+        }
     }
 
 }
---
+++ Assets/Scripts/ScreenSettings.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSettings : MonoBehaviour
+{
+    private Camera mainCam;
+    public float portraitOrientation = 5f;
+    public float landscapeOrientation = 12f;
+
+    private void Start()
+    {
+        mainCam = GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        if (Input.deviceOrientation == DeviceOrientation.LandscapeLeft || Input.deviceOrientation == DeviceOrientation.LandscapeRight)
+        {
+            mainCam.orthographicSize = landscapeOrientation;
+            Debug.Log("LAND");
+        }
+        else if (Input.deviceOrientation == DeviceOrientation.Portrait || Input.deviceOrientation == DeviceOrientation.PortraitUpsideDown)
+            mainCam.orthographicSize = portraitOrientation;
+    }
+}
